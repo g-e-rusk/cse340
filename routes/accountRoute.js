e2ee3f9c19e47e27e07a674fa3faf47d9ea5b6a2
g-e-rusk <grusk92@gmail.com>
2024-01-30 18:11:55 -0800

    form and validation commit

---
+++ routes/accountRoute.js
@@ -0,0 +1,20 @@
+// Needed Resources
+const express = require("express")
+const router = new express.Router()
+const utilities = require("../utilities/")
+const accountController = require("../controllers/accountController")
+const regValidate = require("../utilities/account-validation")
+
+// Route to build login view
+router.get("/login", utilities.handleErrors(accountController.buildLogin))
+
+// Route to build registration view
+router.get("/register", utilities.handleErrors(accountController.buildRegister))
+
+// Route to process registration activity
+router.post("/register", 
+regValidate.registrationRules(),
+regValidate.checkRegData,
+utilities.handleErrors(accountController.registerAccount))
+
+module.exports = router
--- server.js
+++ server.js
@@ -14,6 +14,35 @@ const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
 const utilities = require("./utilities/")
 const errorController = require("./controllers/errorController")
+const session = require("express-session")
+const pool = require('./database/')
+const accountRoute = require("./routes/accountRoute")
+const bodyParser = require("body-parser")
+
+/* ***********************
+ * Middleware
+ *************************/
+app.use(session ({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+
+// Process Registration
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true})) //for parsing application/x-www-form-urlencoded
 
 /* ***********************
  * View Engine and Templates
@@ -33,6 +62,8 @@ app.get("/", utilities.handleErrors(baseController.buildHome))
 app.use("/inv", utilities.handleErrors(inventoryRoute))
 // Error route
 app.use("/error", utilities.handleErrors(errorController.generateError))
+// Login Route
+app.use("/account", utilities.handleErrors(accountRoute))
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
   next({status: 404, message: 'We are unable to locate what you are looking for.'})
@@ -41,7 +72,7 @@ app.use(async (req, res, next) => {
 /* ***********************
  * Express Error Handler
  * Place after all other middleware
- * *********************** */
+ * ***********************/
 app.use(async (err, req, res, next) => {
   let nav = await utilities.getNav()
   console.error(`Error at: "${req.originalUrl}": ${err.message}`)
